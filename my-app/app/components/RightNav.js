9a8956fda8c2f5bcafcb0c64daea3d6bf39ae30d
pshhhhhhhhhh <97819304+pshhhhhhhhhh@users.noreply.github.com>
2024-05-06 19:32:11 +0900

    240506
    상세정보 페이지
    가게 클릭했을때 정보 렌더링 적용

--- my-app/app/components/RightNav.js
+++ my-app/app/components/RightNav.js
@@ -10,12 +10,12 @@ const RightNav = () => {
         return state.rightNavToggle.value;
     });
 
-    const Info = useAppSelector(state => { 
+    const Info = useAppSelector(state => {
         console.log("잘 들어옴? " + JSON.stringify(state.rightNavToggle.regionInfo));
         return state.rightNavToggle.regionInfo;
     });
 
-    const store1 = useAppSelector(state => { 
+    const store1 = useAppSelector(state => {
         return state.rightNavToggle.regionInfo.store1;
     });
     const store1type = useAppSelector(state => {
@@ -35,43 +35,71 @@ const RightNav = () => {
         return state.rightNavToggle.regionInfo.store1phone;
     });
 
-    const store2 = useAppSelector(state => { 
+    const store2 = useAppSelector(state => {
         return state.rightNavToggle.regionInfo.store2;
     });
     const store2type = useAppSelector(state => {
         return state.rightNavToggle.regionInfo.store2type;
     });
+    const store2address = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store2address;
+    });
+    const store2address2 = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store2address2;
+    });
+    const store2time = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store2time;
+    });
+    const store2phone = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store2phone;
+    });
 
-    const store3 = useAppSelector(state => { 
+    const store3 = useAppSelector(state => {
         return state.rightNavToggle.regionInfo.store3;
     });
     const store3type = useAppSelector(state => {
         return state.rightNavToggle.regionInfo.store3type;
     });
+    const store3address = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store3address;
+    });
+    const store3address2 = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store3address2;
+    });
+    const store3time = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store3time;
+    });
+    const store3phone = useAppSelector(state => {
+        return state.rightNavToggle.regionInfo.store3phone;
+    });
 
 
 
 
-    const [slideNum, setSlideNum] = useState(1);
 
 
-    function slideLeft(){
-        if(slideNum > 1){
-        setSlideNum(slideNum-1)
+
+    const [slideNum, setSlideNum] = useState(1); //이미지 슬라이드 state
+
+
+    function slideLeft() { //이미지 슬라이드 함수
+        if (slideNum > 1) {
+            setSlideNum(slideNum - 1)
         }
-        else{
+        else {
             return setSlideNum(3)
         }
     }
-    function slideRight(){
-        if(slideNum < 3){
-        setSlideNum(slideNum+1)
+    function slideRight() {
+        if (slideNum < 3) {
+            setSlideNum(slideNum + 1)
         }
-        else{
+        else {
             return setSlideNum(1)
         }
     }
 
+    const [storeNum, setStoreNum] = useState(1); // 가게 선택 정보를 바꾸기 위한 state
 
 
     return (
@@ -105,41 +133,61 @@ const RightNav = () => {
                 <div className="slide-next-btn" onClick={() => slideLeft()}>
                     <i className="fas fa-angle-left fa-lg fa-1.5x"></i>
                 </div>
-                <div style={{flexGrow:"2"}}></div>
-                <div  className="slide-next-btn" onClick={() => slideRight()}>
+                <div style={{ flexGrow: "2" }}></div>
+                <div className="slide-next-btn" onClick={() => slideRight()}>
                     <i className="fas fa-angle-right fa-lg fa-1.5x" ></i>
                 </div>
             </div>
 
             <div className="slide-btn-wrap">
-                <a className="slide-btn" style={{backgroundColor:slideNum === 1 ? "white": "rgb(155, 156, 158)"}}
-                onClick={() => setSlideNum(1)}></a>
-                <a className="slide-btn" style={{backgroundColor:slideNum === 2 ? "white": "rgb(155, 156, 158)"}}
-                onClick={() => setSlideNum(2)}></a>
-                <a className="slide-btn" style={{backgroundColor:slideNum === 3 ? "white": "rgb(155, 156, 158)"}}
-                onClick={() => setSlideNum(3)}></a>
+                <a className="slide-btn" style={{ backgroundColor: slideNum === 1 ? "white" : "rgb(155, 156, 158)" }}
+                    onClick={() => setSlideNum(1)}></a>
+                <a className="slide-btn" style={{ backgroundColor: slideNum === 2 ? "white" : "rgb(155, 156, 158)" }}
+                    onClick={() => setSlideNum(2)}></a>
+                <a className="slide-btn" style={{ backgroundColor: slideNum === 3 ? "white" : "rgb(155, 156, 158)" }}
+                    onClick={() => setSlideNum(3)}></a>
             </div>
 
             <div className="Rtitle-wrap">
-                <div className="Rtitle">
+                <div className="Rtitle" onClick={() => setStoreNum(1)}>
                     <span className="Rtitle-name">{store1}</span>
                     <span className="Rtitle-sub">{store1type}</span>
                 </div>
-                <div className="Rtitle">
+                <div className="Rtitle" onClick={() => setStoreNum(2)}>
                     <span className="Rtitle-name">{store2}</span>
                     <span className="Rtitle-sub">{store2type}</span>
                 </div>
-                <div className="Rtitle">
+                <div className="Rtitle" onClick={() => setStoreNum(3)}>
                     <span className="Rtitle-name">{store3}</span>
                     <span className="Rtitle-sub">{store3type}</span>
                 </div>
             </div>
             <div className="Rcontent-wrap">
                 <div className="Rcontent">
-                    <span>{store1address}</span>
-                    <span>{store1address2}</span>
-                    <span>{store1time}</span>
-                    <span>{store1phone}</span>
+                    {storeNum === 1 && (
+                        <>
+                            <span>{store1address}</span>
+                            <span>{store1address2}</span>
+                            <span>{store1time}</span>
+                            <span>{store1phone}</span>
+                        </>
+                    )}
+                    {storeNum === 2 && (
+                        <>
+                            <span>{store2address}</span>
+                            <span>{store2address2}</span>
+                            <span>{store2time}</span>
+                            <span>{store2phone}</span>
+                        </>
+                    )}
+                    {storeNum === 3 && (
+                        <>
+                            <span>{store3address}</span>
+                            <span>{store3address2}</span>
+                            <span>{store3time}</span>
+                            <span>{store3phone}</span>
+                        </>
+                    )}
                 </div>
             </div>
             <Rbtn />
